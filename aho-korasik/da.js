1a5b37bb865452b0e8e8c9acfe929d14594b56ca
d.starkov <d.starkov@ideco.ru>
2016-11-23 09:55:26 +0500

    refactor da

--- aho-korasik/da.js
+++ aho-korasik/da.js
@@ -1,42 +1,44 @@
-var fso = new ActiveXObject("Scripting.FileSystemObject")
-var ts  = fso.OpenTextFile("in.txt")
-var S=ts.ReadAll()
-t=WScript.StdIn.ReadLine()
-var m=t.length
-var n=S.length
-var status=0
-alph=new Array()
-var count=0
-var result=new Array()
-
-for(i=0;i<m;i++)
-alph[t.charAt(i)]=0
-del=new Array(m+1)
-for(j=0;j<=m;j++)
-del[j]=new Array()
-for(i in alph)
-del[0][i]=0
-for(j=0;j<m;j++){
-prev=del[j][t.charAt(j)]
-del[j][t.charAt(j)]=j+1
-for(i in alph)
-del[j+1][i]=del[prev][i]
+function readInput() {
+    var fso = new ActiveXObject('Scripting.FileSystemObject');
+    var file  = fso.OpenTextFile('in.txt');
+    var input = file.ReadAll();
+    file.Close();
+    return input;
 }
 
-for (var i=0; i<n;i++)
-{
-  if (del[status][S.charAt(i)]==undefined)
-  {
-    status=0
-    continue
-  }
-  status= del[status][S.charAt(i)]
-  if (status==m) 
-  {
-    result[count]=i-(m-1)
-    count++  
-  } 
-}
+var string = readInput();
+var substring = 'abc';
+var substringLen = substring.length;
+var states = substring.split('');
 
-var res= result.join(", ")
-WScript.echo(res)
+dka = substring.map(function () {
+    return [];
+});
+
+states.forEach(function (state) {
+    dka[0][state] = 0;
+});
+
+states.forEach(function (c, i) {
+    dka[i][c] = i + 1;
+    states.forEach(function (state) {
+        dka[i + 1][state] = dka[dka[i][c]][state];
+    });
+});
+
+var result = [];
+var status = 0;
+
+string.split('').forEach(function (c, i) {
+    if (!dka[status][c]) {
+        status = 0;
+        return;
+    }
+    status = dka[status][c];
+
+    if (status === substringLen) {
+        result.push(i - (substringLen - 1));
+    }
+});
+
+WScript.echo(result.join(', '));
