e17104a70d3e07bd6b783ac393f2539573c5b477
d.starkov <d.starkov@ideco.ru>
2016-11-23 09:05:23 +0500

    Refactoring entropy

--- entropy/ent.js
+++ entropy/ent.js
@@ -1,41 +1,41 @@
-var FSO = new ActiveXObject("Scripting.FileSystemObject");  
-t = FSO.OpenTextFile("Ввод.txt");
-var str = t.ReadAll()
-t.close();
-entrop=0;
-countPovtor=1;
-alph=new Array();
-pop=new Array();
- for(i=0; i<str.length; i++)
-  alph[i]=str.charAt(i);
- for(i=0;i<alph.length-1;i++)
-  {
-    for(k=i+1; k<alph.length; k++)
-	 if(alph[i]==alph[k]&&alph[i]!="")
-	  {
-	   alph[k]="";
-	   countPovtor++;
-	  }
-	if(alph[i]!="")
-	  pop[i]=countPovtor;
-	  else
-	  pop[i]=0;
-	 countPovtor=1;
-  } 
-   if(alph[alph.length-1]!="")
-    pop[alph.length-1]=countPovtor;
-	sum=0;
-	for(f=0;f<pop.length; f++)
-	 if(pop[f]!=0)
-	  sum+=pop[f];
-   for(i=0;i<alph.length; i++)
-   if(alph[i]!="")
-    WScript.echo("simvol: "+alph[i]+" chastota: "+pop[i]/sum);
-	for(i=0;i<pop.length; i++)
-	if(pop[i]!=0)
-	 entrop+=-pop[i]*Math.log(pop[i]/sum)/(Math.log(2)*sum)
-	 WScript.echo("entropiya: "+entrop);
-	 
-	  
-	  
-	 
+function readInput() {
+    var fso = new ActiveXObject('Scripting.FileSystemObject');
+    var file = fso.OpenTextFile('input.txt');
+    var input = file.ReadAll();
+    file.close();
+    return input;
+}
+
+var input = readInput();
+var chars = input.split('');
+var pop = [];
+
+for(var i = 0; i < chars.length - 1; i++) {
+    var repeatCount = 1;
+    for (var k = i + 1; k < chars.length; k++) {
+        if (chars[i] == chars[k] && chars[i] != '') {
+            chars[k] = '';
+            repeatCount++;
+        }
+    }
+
+    pop.push(chars[i] !== '' ? repeatCount : 0);
+}
+
+if (chars[chars.length - 1] !== '') {
+    pop[chars.length - 1] = repeatCount;
+}
+
+var sum = pop.reduce(function (aggregate, element) {
+    return aggregate + element;
+}, 0);
+
+chars.forEach(function (c, i) {
+    if (c !== '') {
+        WScript.echo('Символ: ' + c + ' Частота: ' + pop[i] / sum);
+    }
+
+    WScript.echo('Энтропия: ' + pop.reduce(function (calculated, next) {
+        return calculated + -next * Math.log(next / sum) / (Math.log(2) * sum);
+    }));
+});
